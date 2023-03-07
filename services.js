65b9aaf5208394609f8edaff60f3e7b39a239bc8
jia-wink <1847949884@qq.com>
2023-03-07 18:01:58 +0800

    feat: 优化了接口逻辑

--- services.js
+++ services.js
@@ -1,8 +1,7 @@
 const db = require("./db.js");
 let mysql = require("mysql");
 const nodemailer = require("nodemailer");
-// 邮箱发送的验证码状态，验证码是否匹配
-var codetrue = 0;
+
 exports.start = (req, res) => {};
 // 登录注册处理，向外暴露
 exports.login = (req, res) => {
@@ -28,34 +27,34 @@ exports.register = (req, res) => {
   let username = req.body.username;
   let pwd = req.body.password;
   let code = req.body.code;
+  // 邮箱发送的验证码状态，验证码是否匹配
+  let codetrue = 0;
   // 查询语句
   let sql = "select * from Login where username = ?;";
   // 向表中更改数据
-  let sqlpassword = "update Login set password = ?";
+  let sqlpassword = "update Login set password = ? where username = ?";
 
   db.base(sql, username, (result, error) => {
-    if (!result.length) {
-      return res.json({ status: 0, msg: "请输入邮箱" });
+    // if (!result.length) {
+    //   return res.json({ status: 0, msg: "请输入邮箱" });
+    // } else {
+    if (result[0].code == code) {
+      console.log("codetrue:", codetrue);
+      console.log("匹配验证码...");
+      db.base(sqlpassword, [pwd, username], (result, error) => {
+        console.log("成功了", result);
+        return res.json({ status: 1, msg: "注册成功a" });
+      });
     } else {
-      if (result[0].code == code) {
-        console.log("注册信息:", result);
-        return (codetrue = 1);
-      }
-
       return res.json({ status: 0, msg: "验证码错误" });
     }
+    // }
   });
-  if (codetrue === 1) {
-    console.log("匹配验证码...");
-    db.base(sqlpassword, pwd, (result, error) => {
-      console.log("成功了", result);
-      return res.json({ status: 1, msg: "注册成功a" });
-    });
-  }
 };
 exports.code = (req, res) => {
   // 默认验证码就是初始密码
   let email = req.body.username;
+
   // 产生一个6位的随机数
   let code = Math.floor(Math.random() * 900000) + 100000;
   let pwd = code;
@@ -75,8 +74,8 @@ exports.code = (req, res) => {
     subject: "这里是注册的验证码！",
     //这里就是邮件的主题内容，模板字符串``里可以引入js变量，这里引入的是前面生成的验证码。
     html: `<div>
-          <h2>${code}</h2>
-          </div>`,
+        <h2>${code}</h2>
+        </div>`,
   };
 
   transport.sendMail(options, function (err, msg) {
@@ -87,19 +86,35 @@ exports.code = (req, res) => {
       transport.close;
     }
   });
-  // 把邮箱、密码、验证码全部存入mysql
-  let sql = "insert into Login values (?,?,?)";
-  try {
-    db.base(sql, [email, pwd, code], (result, error) => {
-      if (error) {
-        console.log(result);
-        console.log(error + "hhhhh");
-        return res.json({ status: 0, msg: error.sqlMessage, code });
-      } else {
-        return res.json({ status: 1, msg: "发送验证码成了" });
+
+  let sql1 = "select username from Login where username = ?";
+  db.base(sql1, email, (result, error) => {
+    console.log("查询是否有这个账号", result);
+    if (result[0]) {
+      let sql2 = "update Login set code = ? where username = ?";
+      try {
+        db.base(sql2, [code, email], (result, error) => {
+          return;
+        });
+      } catch (err) {
+        console.log("出错了", err);
       }
-    });
-  } catch (err) {
-    console.log("出错了", err);
-  }
+      return res.json({ status: 0, msg: "这个邮箱已经注册了！继续注册可以修改密码" });
+    } else {
+      // 把邮箱、密码、验证码全部存入mysql
+      let sql = "insert into Login values (?,?,?)";
+      try {
+        db.base(sql, [email, pwd, code], (result, error) => {
+          if (error) {
+            console.log("result", result);
+            return res.json({ status: 0, msg: error.sqlMessage, code });
+          } else {
+            return res.json({ status: 1, msg: "发送验证码成了" });
+          }
+        });
+      } catch (err) {
+        console.log("出错了", err);
+      }
+    }
+  });
 };
